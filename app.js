32e0403252db114bcd8481b823a03192a5d5d8b4
penn <pennsong07@gmail.com>
2015-05-05 13:50:57 +0800

    add Procfile

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var app = express();
 var mongoose = require('mongoose');
 
 var db = mongoose.connection;
-mongoose.connect('mongodb://localhost/HX6');
+//mongoose.connect('mongodb://localhost/HX6');
+mongoose.connect('mongodb://pp:tcltcl@ds033607.mongolab.com:33607/hx');
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
     console.log("db connect open!");
